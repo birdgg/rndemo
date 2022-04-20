db3b655cdbf17af5e4b08727f03bb8c19fce4c74
birdgg <birdeggegg@gmail.com>
2022-04-20 09:44:03 +0800

    init

---
+++ App.tsx
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  Header,
+  LearnMoreLinks,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const array = new Array(10).fill(1);
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Text testID="hello">Hello, Detox!</Text>
+        {array.map((item, index) => (
+          <Button
+            key={index}
+            title={`button ${index}`}
+            onPress={() => {}}
+            testID={`button_${index}`}
+          />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ e2e/s.e2e.js
@@ -0,0 +1,4 @@
+describe('Recorded suite', () => {
+	it('My Recorded Test', async () => {
+	})
+});
