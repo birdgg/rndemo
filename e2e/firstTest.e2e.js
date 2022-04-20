39344496333f02c7b4c70cfedccda38d22170559
birdgg <birdeggegg@gmail.com>
2022-04-20 17:14:45 +0800

    fix: eslint

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,13 +1,13 @@
 describe('App', () => {
   beforeAll(async () => {
-    await device.launchApp();
-  });
+    await device.launchApp()
+  })
 
   beforeEach(async () => {
-    await device.reloadReactNative();
-  });
+    await device.reloadReactNative()
+  })
 
   it('should show the hello message', async () => {
-    await expect(element(by.id('hello'))).toBeVisible();
-  });
-});
+    await expect(element(by.id('hello'))).toBeVisible()
+  })
+})
