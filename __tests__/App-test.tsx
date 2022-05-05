fe8247075c7620e547223698c15a4594aea10511
birdgg <birdeggegg@gmail.com>
2022-05-05 11:13:34 +0800

    feat: add ui test library

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,29 @@
-/**
- * @format
- */
+import * as React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import {render, fireEvent} from '@testing-library/react-native'
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import App from '../App'
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('Testing react navigation', () => {
+  test('find home screen text', async () => {
+    const component = <App />
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+    const {findByText} = render(component)
+
+    const header = await findByText('Home Screen')
+
+    expect(header).toBeTruthy()
+  })
+
+  test('setting tab click', async () => {
+    const component = <App />
+
+    const {findByText, findByTestId} = render(component)
+    const toClick = await findByTestId('settingsTab')
+
+    fireEvent(toClick, 'press')
+    const settings = await findByText('Settings!')
+
+    expect(settings).toBeTruthy()
+  })
+})
